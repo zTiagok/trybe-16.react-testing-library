5d7fbba482abb4a64ce89ca60734b65f834181cc
Tiago <ztiagok@gmail.com>
2022-07-18 18:38:38 -0500

    c4 - 4º Requisito Completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,42 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testings in the "Page Not Found" component. It...', () => {
+  it('Should have a "H2" with a "page not found" message.',
+    () => {
+      const customHistory = createMemoryHistory();
+      render(
+        <Router history={ customHistory }>
+          <App />
+        </Router>,
+      );
+
+      customHistory.push('/error');
+
+      const screenMessage = screen.getByRole('heading', {
+        name: /page requested not found/i,
+      });
+
+      expect(screenMessage).toBeInTheDocument();
+    });
+
+  it('Should render a GIF when entering the page.',
+    () => {
+      const customHistory = createMemoryHistory();
+      render(
+        <Router history={ customHistory }>
+          <App />
+        </Router>,
+      );
+
+      customHistory.push('/error');
+
+      const screenImage = document.querySelector('.not-found-image');
+
+      expect(screenImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
